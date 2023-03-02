6573777a765b4b24b450e4e66a1ba674f609fd75
Nese84 <nbserbest84@gmail.com>
2023-03-02 12:40:43 +0300

    doctors

--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -1,2 +1,32 @@
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
 import { AddModal } from "./AddModal"
 import { useState } from "react"
+import { Image } from "react-bootstrap"
+
+const Doctors =({doctors})=> {
+    return(
+        <Container className="p-2">
+            <h3 className="display-6" style={{ color:"rgb(166, 18, 189"}}> Our Doctors </h3>
+
+
+            <Row>
+                {doctors.map ((dr)=>(
+                    <Col>
+                    <img src={dr.img} alt={dr.name} />
+                    <h4>{dr.name}</h4>
+                    <h5>{dr.dep}</h5>
+                       
+                    </Col>
+                ) )}
+                
+            </Row>
+        </Container>
+
+    )
+}
+
+
+
+export default Doctors

d4002c0c9f1f9e129750c3dc6c651bf446362a72
Nese84 <nbserbest84@gmail.com>
2023-03-03 01:10:35 +0300

    appointment

--- src/components/AppointmentList.jsx
+++ src/components/AppointmentList.jsx
@@ -2,3 +2,17 @@ import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 import { FaTimesCircle } from "react-icons/fa"
+
+const AppointmentList = ({ appointments, setAppointments }) => {
+    const handleDelete = (id) => {
+      setAppointments(appointments.filter((item) => item.id !== id))
+    }
+  
+    const handleDoubleClick = (id) => {
+      setAppointments(
+        appointments.map((item) =>
+          item.id === id ? { ...item, consulted: !item.consulted } : item
+        )
+      )
+    }
+  
